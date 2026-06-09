15484bc2b75f695eb3ddd97130207c3a3f664895
Miguel Arcanjo <103754398+ArcanjoDev7@users.noreply.github.com>
2026-06-09 20:43:25 -0300

    Add delete movement use case

---
+++ src/main/kotlin/com/arcanjodev/cofrinho/application/usecase/DeleteMovementUseCase.kt
@@ -0,0 +1,17 @@
+package com.arcanjodev.cofrinho.application.usecase
+
+import com.arcanjodev.cofrinho.application.exception.MovementNotFoundException
+import com.arcanjodev.cofrinho.domain.model.MovementId
+import com.arcanjodev.cofrinho.domain.repository.MovementRepository
+
+class DeleteMovementUseCase(
+    private val movementRepository: MovementRepository
+) {
+    fun execute(id: String) {
+        val deleted = movementRepository.deleteById(MovementId(id))
+
+        if (!deleted) {
+            throw MovementNotFoundException("Movimentacao nao encontrada")
+        }
+    }
+}

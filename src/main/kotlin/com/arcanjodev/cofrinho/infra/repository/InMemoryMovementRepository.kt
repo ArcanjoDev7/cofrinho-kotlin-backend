1f552af56d736cfcb114e5b1f3d69cd792ecfc16
Miguel Arcanjo <103754398+ArcanjoDev7@users.noreply.github.com>
2026-06-09 20:43:29 -0300

    Add in-memory repository

---
+++ src/main/kotlin/com/arcanjodev/cofrinho/infra/repository/InMemoryMovementRepository.kt
@@ -0,0 +1,19 @@
+package com.arcanjodev.cofrinho.infra.repository
+
+import com.arcanjodev.cofrinho.domain.model.Movement
+import com.arcanjodev.cofrinho.domain.model.MovementId
+import com.arcanjodev.cofrinho.domain.repository.MovementRepository
+import java.util.concurrent.ConcurrentHashMap
+
+class InMemoryMovementRepository : MovementRepository {
+    private val movements = ConcurrentHashMap<String, Movement>()
+
+    override fun findAll(): List<Movement> = movements.values.toList()
+
+    override fun save(movement: Movement): Movement {
+        movements[movement.id.value] = movement
+        return movement
+    }
+
+    override fun deleteById(id: MovementId): Boolean = movements.remove(id.value) != null
+}
